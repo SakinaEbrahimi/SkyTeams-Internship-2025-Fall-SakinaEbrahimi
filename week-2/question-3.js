2b023e1f43be052e0071eccced947e5289e779f2
Sakina Ebrahimi <sakinaebrahimi9@gmail.com>
2025-11-14 02:16:32 -0800

    Initila function area and perimeter rectangle with test

--- week-2/question-3.js
+++ week-2/question-3.js
@@ -2,8 +2,13 @@
     // Author: Sakina Ebrahimi
     //Description: Calculate the area and perimeter of a rectangle
 
+
 //function area
 function areaRectangle(width, length){
+    if( width <= 0 || length <= 0){
+        console.log("Number couldnot be zero or negative")
+        return false
+    }
    return (width*length)
 }
 
