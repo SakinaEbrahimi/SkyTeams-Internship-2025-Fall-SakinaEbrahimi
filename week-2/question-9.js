ffa549310d69e1e43c360787107d0fc93960e677
Sakina Ebrahimi <sakinaebrahimi9@gmail.com>
2025-11-16 02:12:10 -0800

    Add function grade calculator

---
+++ week-2/question-9.js
@@ -0,0 +1,24 @@
+
+    //Author: Sakina Ebrahimi
+    //Description: Grade Calculator(0-100 -> A-F): input a student's marks (0-100) and print the grade on scale.
+
+module.exports = function gradeCalculator(mark){
+
+    if( mark < 0 || mark > 100){
+        throw new Error('Invalid Marks')
+    }
+
+    if( mark <= 100 && mark >= 90){
+        return 'A'
+    }else if( mark < 90 && mark >= 80 ){
+        return 'B'
+    }else if( mark < 80 && mark >= 70){
+        return 'C'
+    }else if( mark < 70 && mark >= 60){
+        return 'D'
+    }else if( mark < 60 && mark >= 50){
+        return 'E'
+    }else{
+        return 'F'
+    }
+}
