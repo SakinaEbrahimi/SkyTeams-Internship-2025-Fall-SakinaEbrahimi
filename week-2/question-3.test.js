820b3f5adacd9841b876aaed3d680a2a504b0aa4
Sakina Ebrahimi <sakinaebrahimi9@gmail.com>
2025-11-16 01:14:46 -0800

    Update test

--- week-2/question-3.test.js
+++ week-2/question-3.test.js
@@ -2,11 +2,13 @@
     // Author: Sakina Ebrahimi
     //Description: Test Calculate the area and perimeter of a rectangle
 
+const { default: expect } = require('expect')
 const {areaRectangle, perimeterRectangle } = require('./question-3')
 
 describe('Test for area rectangle function',()=>{
     it('return area when width and length is greater than zero',()=>{
         expect(areaRectangle(3,4)).toBe(12)
+        expect(areaRectangle(10,20)).toBe(200)
     })
     it('return an error when width or length is zero', ()=>{
         expect(()=>{areaRectangle(0,4)}).toThrow()
@@ -21,6 +23,7 @@ describe('Test for area rectangle function',()=>{
 describe('Test perimeter rectangle function', ()=>{
     it('return perimeter when width and length is greater than zero', ()=>{
         expect(perimeterRectangle(3,4)).toBe(14)
+        expect(perimeterRectangle(10,20)).toBe(60)
     })
     it('return an error when length or width is zero',()=>{
         expect(()=>{perimeterRectangle(0,4)}).toThrow()
