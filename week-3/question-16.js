dc08f0e8fb010247c79a0acde6a6be382550d73b
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2025-11-26 13:13:36 -0800

    function Fibonacci

---
+++ week-3/question-16.js
@@ -0,0 +1,17 @@
+
+    // Author: Sakina Ebrahimi
+    // Description: Fibonacci sequence
+    //input:7
+    //output: 0,1,1,2,3,5,8
+
+
+function Fibonacci( num ){
+    if( num === null || num === undefined || typeof num !== 'number' || num===0) throw new Error('Invalid input')
+
+    let fibonacci = [0,1]
+    for(i=2; i<num; i++){
+        fibonacci[i] = fibonacci[i-1]+ fibonacci[i-2]
+    }
+    return fibonacci
+}
+module.exports = Fibonacci
