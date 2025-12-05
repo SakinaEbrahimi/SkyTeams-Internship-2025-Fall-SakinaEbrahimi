663c517a19aaa9b21b6ec275e7a06dd4827e5eac
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2025-12-05 06:50:36 -0800

    Implement queue using two stack

---
+++ week-4/question-19.test.js
@@ -0,0 +1,35 @@
+  
+    // Author: SakinaEbrahimi
+    // Description: Implement a stack (array based)
+    //operations: push, pop, peek, isempty
+
+const {enqueue, dequeue, peek} = require('./question-19')
+
+describe('Test enqueu function ',()=>{   
+    it('test enqueu function',()=>{
+        expect( enqueue(1)).toEqual([1])
+        expect( enqueue(2)).toEqual([1,2])
+        expect( enqueue(3)).toEqual([1,2,3])
+        expect( enqueue(4)).toEqual([1,2,3,4])
+        expect( enqueue('hello')).toEqual([1,2,3,4,'hello'])
+    })
+    
+})
+
+
+describe('Test dequeue function ',()=>{
+    
+    it('test dequeue function',()=>{
+        expect(dequeue()).toEqual(1)
+        expect(dequeue()).toEqual(2)
+        expect(dequeue()).toEqual(3)
+        expect(dequeue()).toEqual(4)
+
+    })
+        
+})
+describe('Test peek function ',()=>{
+    it('return top element of stack',()=>{
+        expect(peek()).toBe('hello')
+    })
+})
