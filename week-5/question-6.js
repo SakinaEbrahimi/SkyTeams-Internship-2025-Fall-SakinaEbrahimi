df6319c1d0408e97567d1a04b4ecf20cc63f3056
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2025-12-11 03:50:08 -0800

    Implement Adjacent Duplicates

---
+++ week-5/question-6.js
@@ -0,0 +1,25 @@
+
+    //Author: Sakina Ebrahimi
+    //Remove Adjacent Duplicates
+    //Given a string s, repeatedly remove adjacent pairs of equal characters
+    //input: abbaca
+    //output: ca
+
+const Stack = require('./Stack')
+
+function removeDuplicates(str){
+    if(typeof str !== 'string' || str === null || str === undefined) throw new Error('invalid input')
+
+    const stack = new Stack()
+    let s = ''
+    for(i=0; i<str.length; i++){
+        if(str[i] === stack.peek()){
+            stack.pop()
+        }else{
+            stack.push(str[i])
+        }
+    }    
+    return stack.reverse()
+}
+
+module.exports = removeDuplicates
